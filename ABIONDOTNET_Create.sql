c90384a9197b41d09db5b749ca6c63d89d5d6d89
Michael G. Workman <46582611+Michael-G-Workman@users.noreply.github.com>
2019-01-14 16:29:28 -0500

    Minor Text Changes

--- ABIONDOTNET_Create.sql
+++ ABIONDOTNET_Create.sql
@@ -7,10 +7,6 @@ By Michael G. Workman
 
 This script will create the AbionDotNet database to be used with the abion.net mvc web application.
 
-AZURE NOTE: Normally a sql server database cannot be created with a SQL script in Azure, in the case of
-using this script with an Azure DB, remove the create database statement. Azure SQL Server databases
-can only be created in the online Portal.
-
 This example is freely distributable under terms of MIT open source license
 
 =========================================================================================================
